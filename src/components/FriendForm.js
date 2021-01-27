ac2117b5a6d222845cf63e6cf9bf789c0e7cec44
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-27 11:24:29 -0600

    wip

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -79,7 +79,7 @@ export default function FriendForm(props) {
           <input
             type="radio"
             name="civil"
-            value={values.civil}
+            value={values.civil === "single"}
             checked={true}
           />
         </label>
