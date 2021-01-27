033308a2d6a162bf3c74e1661220deef03bf1f08
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-27 11:53:52 -0600

    wip

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -10,7 +10,9 @@ export default function FriendForm(props) {
 
   const onChange = (evt) => {
     /* 🔥 FIX THIS SO IT ALSO WORKS WITH CHECKBOXES */
-  
+    const { name, value, type, checked } = evt.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    change(name, valueToUse);
   };
 
   return (
